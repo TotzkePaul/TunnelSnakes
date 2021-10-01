26451f18250514ef0619938ab9cb5628af77dfca
Paul Totzke <7072k3@gmail.com>
2021-09-30 22:06:54 -0500

    dont chase tail when too much food

--- server_logic.py
+++ server_logic.py
@@ -127,7 +127,7 @@ def choose_direction(my_head: Dict[str, int],  my_body: List[dict], my_health:in
         print("Only one move possible")
         return possible_moves[0]
 
-    if (len(food) == 0 or len(food)> len(free_spots)) or (my_health > 33 and len(my_body) > 5):
+    if len(food) == 0 or (len(food) < len(free_spots) and my_health > 33 and len(my_body) > 5):
         my_tail = my_body[-1]
 
         tail_adj = [(my_tail["x"] + x[0], my_tail["y"]+x[1] ) for x in [(0, -1), (0, 1), (-1, 0), (1, 0)]]
