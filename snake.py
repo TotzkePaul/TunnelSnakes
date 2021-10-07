b060a927ce067b003702011b68557721a844d079
Paul Totzke <7072k3@gmail.com>
2021-10-06 22:29:33 -0500

    fix extra attribute color

--- snake.py
+++ snake.py
@@ -2,13 +2,11 @@
 class Snake: 
     def __init__(self, snake_data):
         self.id = snake_data['id']
-        self.name = snake_data['name']
-        self.length = len(snake_data['body'])
-        self.body = snake_data['body']
-        self.head = snake_data['body'][0]
-        self.health :int = snake_data['health']
-        self.color = snake_data['color']
+        self.name = snake_data['name']        
         self.head = snake_data['head']
+        self.body = snake_data['body']
+        self.length = len(snake_data['body'])
+        self.health :int = snake_data['health']        
         self.latency:int = snake_data['latency']
 
 
