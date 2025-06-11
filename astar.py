0216cb95abe926dc49970e123c09c31024f57c8b
Paul Totzke <7072k3@gmail.com>
2025-06-10 22:44:15 -0500

    Fix pathfinding and neck logic

--- astar.py
+++ astar.py
@@ -1,16 +1,17 @@
-class Node():
+class Node:
     """A node class for A* Pathfinding"""
 
     def __init__(self, parent=None, position=None):
         self.parent = parent
         self.position = position
 
-        self.g = 0 # General cost: The actual cost path from the start node to the current node. 
-        self.h = 0 # Heuristic cost: The actual cost path from the current node to goal node.
-        self.f = 0 # Final cost: The actual cost path from the start node to the goal node.
+        self.g = 0  # Distance from start node
+        self.h = 0  # Estimated distance to end node
+        self.f = 0  # Total cost
 
     def __eq__(self, other):
         return self.position == other.position
+
 
 def return_path(current_node):
     path = []
@@ -18,88 +19,75 @@ def return_path(current_node):
     while current is not None:
         path.append(current.position)
         current = current.parent
-    return path[::-1]  # Return reversed path
+    return path[::-1]
+
 
 def cost_function(child, end_node, hazard_grid):
-    distance = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+    distance = (
+        (child.position[0] - end_node.position[0]) ** 2
+        + (child.position[1] - end_node.position[1]) ** 2
+    )
     penalty = hazard_grid[child.position[0]][child.position[1]]
     return distance + penalty
-    
+
 
 def astar(maze, start, end, hazard_grid):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
-
-    # Create start and end node
+    """Return a path from start to end in the given maze using A* search."""
     start_node = Node(None, start)
     end_node = Node(None, end)
 
-    # Initialize both open and closed list
-    open_list = []
+    open_list = [start_node]
     closed_list = []
 
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-
-        # Get the current node
-        current_node = open_list[0]
+    while open_list:
         current_index = 0
+        current_node = open_list[0]
         for index, item in enumerate(open_list):
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
 
-        # Pop current off open list, add to closed list
         open_list.pop(current_index)
         closed_list.append(current_node)
 
-        # Found the goal
         if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+            return return_path(current_node)
 
-        # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            node_position = (
+                current_node.position[0] + new_position[0],
+                current_node.position[1] + new_position[1],
+            )
 
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+            if (
+                node_position[0] > len(maze) - 1
+                or node_position[0] < 0
+                or node_position[1] > len(maze[0]) - 1
+                or node_position[1] < 0
+            ):
                 continue
 
-            # Make sure walkable terrain
             if maze[node_position[0]][node_position[1]] != 0:
                 continue
 
-            # Create new node
             new_node = Node(current_node, node_position)
-
-            # Append
             children.append(new_node)
 
-        # Loop through children
         for child in children:
-
-            # Child is on the closed list
             if child in closed_list:
-                    continue
+                continue
 
-            # Create the f, g, and h values
             child.g = current_node.g + 1
             child.h = cost_function(child, end_node, hazard_grid)
             child.f = child.g + child.h
 
-            # Child is already in the open list
-            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
+            if any(
+                open_node for open_node in open_list
+                if child.position == open_node.position and child.g > open_node.g
+            ):
                 continue
 
-            # Add the child to the open list
             open_list.append(child)
+
+    return None
